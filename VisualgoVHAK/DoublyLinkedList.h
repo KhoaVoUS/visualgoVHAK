28ce87a2e83c2792301394ec2888cb1038652ce4
vhakHCMUS <22127201@student.hcmus.edu.vn>
2023-04-05 16:27:38 +0700

    add project files

---
+++ VisualgoVHAK/DoublyLinkedList.h
@@ -0,0 +1,33 @@
+#pragma once
+struct Node {
+	int data = 0;
+	Node* Next = nullptr;
+	Node* Prev = nullptr;
+};
+
+struct doublyLinkedList
+{
+	Node* pHead = nullptr;
+	Node* pTail = nullptr;
+
+	void addTail(Node* node);
+
+	void addHead(Node* node);
+
+	void insertIndexK(int k, int data);
+
+	void deleteTail();
+
+	void deleteHead();
+
+	void deleteIndexK(int k);
+
+	void updateIndexK(int k, int data);
+
+	void loadList();
+};
+
+
+
+Node* create(int data);
+
